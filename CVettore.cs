0947fb22c79a4a7ab1436156979700a5287f8f93
Federico Zhang <116790986+FedericoZhang1801@users.noreply.github.com>
2023-01-19 10:30:29 +0100

    Add files via upload

---
+++ CVettore.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+
+public class Vettore
+{
+
+    private int[] vettore;
+    private int idx = 0;
+    public Vettore()
+    {
+
+        vettore = new int[] { 0 };
+
+    }
+
+    public Vettore(int N)
+    {
+        if (N <= 0) 
+            vettore = new int[0];
+        else    
+            vettore = new int[N];
+
+    }
+
+    public bool Aggiungi(int Val)
+    {
+        if (idx >= vettore.Length)
+            return false;
+
+        vettore[idx] = Val;
+        idx++;
+
+        return true;
+    }
+
+    public int Somma()
+    {
+        int retVal = 0;
+        for (int idx = 0; idx < vettore.Length; idx++)
+        {
+            retVal += vettore[idx];
+        }
+            return retVal;
+    }
+
+    public int Ultimo()
+    {  
+        if(vettore.Length == 0)
+            return 0;
+        if( idx == 0 )
+            return vettore[idx];
+
+        return vettore[idx - 1];
+    }
+
+
+}
